ebe4ed8d258899a9f45cd5e91c3b28b01dfbe3de
Nicolas Degen <nicolas.degen@valora.com>
2024-05-01 04:14:54 -0400

    Move grid items to component

---
+++ components/GridImage.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import { MutableRefObject } from "react";
+
+interface GridImageProps {
+  id: string;
+  href: string;
+  src: string;
+  ref?: MutableRefObject<HTMLAnchorElement>;
+}
+
+export default function GridImage({id, href, src, ref}: GridImageProps) {
+
+  return (
+    <div
+      key="1"
+      // href={`/?photoId=${imageId}`}
+      // as={`/p/${imageId}`}
+      // shallow
+      className="after:content group relative mb-5 block w-full after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+    >
+      <Link
+        key={id}
+        href={href}
+        ref={ref}
+        shallow
+        className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+      >
+        <Image
+          alt="Album Image"
+          className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+          style={{ transform: "translate3d(0, 0, 0)" }}
+          src={src}
+          width={720}
+          height={480}
+          unoptimized={true}
+          //   sizes="(max-width: 640px) 100vw,
+          // (max-width: 1280px) 50vw,
+          // (max-width: 1536px) 33vw,
+          // 25vw"
+        />
+      </Link>
+    </div>
+  );
+};
--- pages/invite/[id].tsx
+++ pages/invite/[id].tsx
@@ -17,6 +17,7 @@ import Bridge from "../../components/Icons/Bridge";
 import { useLastViewedPhoto } from "../../utils/useLastViewedPhoto";
 import Modal from "../../components/Modal";
 import { AlbumItem } from "../../utils/types";
+import GridImage from "../../components/GridImage";
 
 interface PropsData {
   albumImagePreviewURL: string;
@@ -267,39 +268,16 @@ export default function InvitePage(props: PropsData) {
               </button>
             </div> */}
             {props.albumItems.map((albumItem) => (
-              <div
-                key={albumItem.image}
-                // href={`/?photoId=${imageId}`}
-                // as={`/p/${imageId}`}
-                // shallow
-                className="after:content group relative mb-5 block w-full after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-              >
-                <Link
-                  key={albumItem.image}
-                  href={`${router.pathname}?id=${props.inviteId}&imageId=${albumItem.image}`}
-                  ref={
+              <GridImage
+                id={albumItem.image}
+                href={`${router.pathname}?id=${props.inviteId}&imageId=${albumItem.image}`}
+                src={`${props.domain}/api/v1/invites/${props.inviteId}/images/${albumItem.image}/preview`}
+                ref={
                     albumItem.image === lastViewedPhoto
                       ? lastViewedPhotoRef
                       : null
                   }
-                  shallow
-                  className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-                >
-                  <Image
-                    alt="Album Image"
-                    className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                    style={{ transform: "translate3d(0, 0, 0)" }}
-                    src={`${props.domain}/api/v1/invites/${props.inviteId}/images/${albumItem.image}/preview`}
-                    width={720}
-                    height={480}
-                    unoptimized={true}
-                    //   sizes="(max-width: 640px) 100vw,
-                    // (max-width: 1280px) 50vw,
-                    // (max-width: 1536px) 33vw,
-                    // 25vw"
-                  />
-                </Link>
-              </div>
+              />
             ))}
           </div>
         </section>
