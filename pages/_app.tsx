5a6e07cb42d10be6ac7fc6c8b9a67212be537f4d
Nicolas Degen <ndegen@me.com>
2024-08-08 20:24:04 +0200

    Not wokring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,15 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/style.css";
-import DefaultLayout from "@/components/DefaultLayout";
-import { appWithTranslation } from "next-i18next";
-import NoHeaderLayout from "@/components/NoHeaderLayout";
 
-function App({ Component, pageProps, router }: AppProps) {
-  const Layout = router.pathname.includes("/invite")
-    ? NoHeaderLayout
-    : DefaultLayout;
-
+function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <div>
       <Head>
-        <title>Echo Photos</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="apple-itunes-app"
-          content="app-id=1499073049, app-clip-bundle-id=LVS4T6PPZ4.ch.echolabs.echo.Clip, app-clip-display=card"
-        />
-        <meta name="description" content="Supreme Photo sharing" />
+        <title>My App</title>
       </Head>
       <Component {...pageProps} />
-    </Layout>
+    </div>
   );
 }
-
-export default appWithTranslation(App);
+export default App;
