648df922c036a2c52300e7dba170d20c4891e81e
Nicolas Degen <ndegen@me.com>
2025-01-24 11:55:23 -0500

    Merge disclosure code
    Animate warnings

--- components/Warnings/NoLinksAvailable.tsx
+++ components/Warnings/NoLinksAvailable.tsx
@@ -1,26 +1,25 @@
 'use client';
 
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
-import { IoChevronDown, IoWarning } from 'react-icons/io5';
+import { HStack, VStack } from '@components/UI/Components';
+import Disclosure from '@components/UI/Disclosure';
+import { IoWarning } from 'react-icons/io5';
 
 export default function NoLinksAvailable() {
   return (
-    <div className="mx-auto w-full max-w-sm divide-y divide-white/5 rounded-xl bg-white/5">
-      <Disclosure as="div" className="rounded-xl bg-slate-200 p-2">
-        <DisclosureButton className="group flex w-full items-center justify-between text-gray-800 hover:text-gray-600">
-          <span className="flex flex-row text-sm/6 font-medium">
-            <IoWarning size={22} className="mr-2 text-red-500" />
-            No download links available
-          </span>
-          <IoChevronDown size={22} className="group-data-[open]:rotate-180" />
-        </DisclosureButton>
-        <DisclosurePanel className="mt-2 text-start text-sm font-light text-gray-700">
-          <p>This album has used all available download links.</p>
-          <p className="mt-2">
-            To create an updated download link, you will need to upgrade the album to premium.
-          </p>
-        </DisclosurePanel>
-      </Disclosure>
-    </div>
+    <Disclosure
+      title={
+        <HStack className="text-sm/6 font-medium">
+          <IoWarning size={22} className="mr-2 text-red-500" />
+          No download links available
+        </HStack>
+      }
+    >
+      <VStack>
+        <p>This album has used all available download links.</p>
+        <p className="mt-2">
+          To create an updated download link, you will need to upgrade the album to premium.
+        </p>
+      </VStack>
+    </Disclosure>
   );
 }
--- components/Warnings/NoVideoUpload.tsx
+++ components/Warnings/NoVideoUpload.tsx
@@ -1,24 +1,21 @@
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
-import { IoChevronDown, IoInformationCircleOutline } from 'react-icons/io5';
+import { HStack } from '@components/UI/Components';
+import Disclosure from '@components/UI/Disclosure';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 
 export default function NoVideoUploadAvailable() {
   return (
-    <div className="w-full max-w-sm divide-y divide-white/5 rounded-xl bg-white/5">
-      <Disclosure as="div" className="rounded-xl bg-slate-200 p-2">
-        <DisclosureButton className="group flex w-full items-center justify-between text-gray-800 hover:text-gray-600">
-          <span className="flex flex-row text-sm/6 font-medium">
-            <IoInformationCircleOutline size={22} className="mr-2 text-yellow-500" />
-            Upload videos from app
-          </span>
-          <IoChevronDown size={22} className="group-data-[open]:rotate-180" />
-        </DisclosureButton>
-        <DisclosurePanel className="mt-2 text-start text-sm font-light text-gray-700">
-          <p>
-            Video uploads are currently only from the apps. You can install the app both on iOS and
-            Android.
-          </p>
-        </DisclosurePanel>
-      </Disclosure>
-    </div>
+    <Disclosure
+      title={
+        <HStack className="text-sm/6 font-medium">
+          <IoInformationCircleOutline size={22} className="mr-2 text-yellow-500" />
+          Upload videos from app
+        </HStack>
+      }
+    >
+      <p>
+        Video uploads are currently only from the apps. You can install the app both on iOS and
+        Android.
+      </p>
+    </Disclosure>
   );
 }
