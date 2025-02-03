b2f0de65da1fe53c1384714758851546463e4492
Nicolas Degen <ndegen@me.com>
2025-02-03 13:58:28 -0500

    Add error boundary

---
+++ app/(app)/error.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { VStack } from '@components/UI/Components';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <VStack className="w-full h-full items-center justify-center space-y-3">
+      <h1 className="text-4xl font-black">Oops 😵‍💫</h1>
+      <h2 className="text-lg">Something went wrong!</h2>
+      <button
+        className="btn btn-primary"
+        onClick={
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </VStack>
+  );
+}
