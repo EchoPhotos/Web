fc826461d63f398bf27d5c61797eae1d84b0ef5d
Nicolas Degen <ndegen@me.com>
2025-07-14 13:49:09 -0400

    Move config to env

--- utils/FirebaseConfig.ts
+++ utils/FirebaseConfig.ts
@@ -4,34 +4,17 @@ import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
-const firebaseConfigDev: FirebaseOptions = {
-  apiKey: 'AIzaSyALJO67DnPTZr55_d9UspFkXAuwrNFP6eA',
-  authDomain: 'echo-photos-dev.firebaseapp.com',
-  databaseURL: 'https://echo-photos-dev-default-rtdb.firebaseio.com',
-  projectId: 'echo-photos-dev',
-  storageBucket: 'echo-photos-dev.appspot.com',
-  messagingSenderId: '246784474922',
-  appId: '1:246784474922:web:b84e8843d8d4762df66853',
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const firebaseConfigProd: FirebaseOptions = {
-  apiKey: 'AIzaSyArth3HcZgLI3HBVXZfbkRxedyKBfu6z2c',
-  authDomain: 'echo-photos.firebaseapp.com',
-  databaseURL: 'https://echo-photos-default-rtdb.firebaseio.com',
-  projectId: 'echo-photos',
-  storageBucket: 'echo-photos.appspot.com',
-  messagingSenderId: '962144839051',
-  appId: '1:962144839051:web:6fefcc52ab146daf37fb8e',
-};
-
-function currentConfig() {
-  if (process.env.NODE_ENV === 'development') {
-    return firebaseConfigDev;
-  }
-  return firebaseConfigProd;
-}
-
-const app = initializeApp(currentConfig());
+const app = initializeApp(firebaseConfig);
 
 const storage = getStorage(app);
 const auth = getAuth(app);
